b2ae9e04e96c151f5b63de3036107cafc729437f
junhong <junhong@junhongs-MacBook-Air.local>
2018-01-07 11:02:21 +0900

    print hello world for test

--- app/crawler.py
+++ app/crawler.py
@@ -1,2 +1,14 @@
 #Python 3.6.2
 
+class Crawler(object):
+	"""A crawler will bring html source code from web.
+	following properties:
+
+	Attributes:
+		url: target url to crawl 
+	"""
+
+	def __init__(self, url):
+		self.url = url
+
+	
--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 #Python 3.6.2
-from flask import flask
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def callCrawler():
+	return 'Hello World!'
